c606ad7283d0e458593c6e43ccaa2ee6620a4e62
jeRryLeE0201 <108649934+jeRryLeE0201@users.noreply.github.com>
2022-07-07 16:05:42 +0800

    change some words at home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,10 +11,10 @@ const step = ref(0);
         <Typing
             v-if="step >= 0"
             @done="step++"
-            text="My Portfolio"
+            text="Jerry's Website"
             class="mb-2 block text-2xl sm:text-3xl lg:text-4xl"
         />
-        <Typing v-if="step >= 1" @done="step++" text="Hello!" :speed="60" />
+        <Typing v-if="step >= 1" @done="step++" text="Welcome to my website" :speed="60" />
         <a
             v-if="step >= 2"
             class="mt-8 flex items-center text-lg text-gray-500 transition-all hover:text-gray-700"
